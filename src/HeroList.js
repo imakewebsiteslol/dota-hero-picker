9319afa6a85422118a11aae2306cc82804ff666e
jd <imakewebsiteslol@protonmail.com>
2023-09-05 17:13:16 -0400

    applied changes and critique, forgot to commit oops

--- src/HeroList.js
+++ src/HeroList.js
@@ -5,6 +5,11 @@ function HeroList({ heroes, onSelectHero }) {
     <ul className="hero-list">
       {heroes.map(hero => (
         <li key={hero.id} onClick={() => onSelectHero(hero)}>
+          <img
+            src={`https://api.opendota.com${hero.img}`}
+            alt={hero.localized_name}
+            className="hero-image"
+          />
           {hero.localized_name}
         </li>
       ))}
@@ -13,4 +18,5 @@ function HeroList({ heroes, onSelectHero }) {
 }
 
 export default HeroList;
+
 
